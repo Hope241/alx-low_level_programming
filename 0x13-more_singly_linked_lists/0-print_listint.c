f8f9c63dce6f879452739fd8cc7882bccab152e7
Hope241 <tntshang@gmail.com>
2023-04-03 13:56:25 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *node = h;
-	size_t num = 0;
+	size_t cont = 0;
 
 	while (node)
 	{
 		printf("%i\n", node->n);
-		num++;
+		cont++;
 		node = node->next;
 	}
 
-	return (num);
+	return (cont);
 }
