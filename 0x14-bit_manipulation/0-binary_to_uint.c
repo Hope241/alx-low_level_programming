60688cdb5813c7ea14c11cd5c4609fc4232941d3
Hope241 <tntshang@gmail.com>
2023-04-08 08:04:20 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,23 +8,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base = 1, result = 0, len = 0;
+    unsigned int base = 1, result = 0, len = 0;
 
-	if (b == NULL)
-		return (0);
+    if (b == NULL)
+        return (0);
 
-	while (b[len])
-		len++;
+    for (len = 0; b[len] != '\0'; len++)
+    {
+        if (b[len] != '0' && b[len] != '1')
+            return (0);
 
-	while (len)
-	{
-		if (b[len - 1] != '0' && b[len - 1] != '1')
-			return (0);
+        if (b[len] == '1')
+            result += base;
+        base *= 2;
+    }
 
-		if (b[len - 1] == '1')
-			result += base;
-		base *= 2;
-		len--;
-	}
-	return (result);
+    return (result);
 }
+
